5e50f691d976571bbc270e01c1174ff17a1abf17
mnhnielsen <mnhnielsen@gmail.com>
2022-09-01 14:07:48 +0200

    returning child resources

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -15,19 +15,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with the big park"
+                    Description = "The one with the big park",
+                    PoinstOfInterests = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "A big park in a city"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Jerry's Apartment",
+                            Description = "This is were Jerry lives"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Berlin",
-                    Description = "The one with the nice kebabs"
+                    Description = "The one with the nice kebabs",
+                    PoinstOfInterests = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Checkpoint Charlie",
+                            Description = "Berlin Wall checkpoint"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Zaddy's",
+                            Description = "Cheap and good kebab place"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Lucca",
-                    Description = "The one with the fast sprinter"
+                    Description = "The one with the fast sprinter",
+                    PoinstOfInterests = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "City Walls",
+                            Description = "The wall around the city"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "Hills",
+                            Description = "Hills/mountains overlooking the coast"
+                        }
+                    }
                 }
             };
         }
--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -5,5 +5,13 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PoinstOfInterests.Count;
+            }
+        }
+        public ICollection<PointOfInterestDto> PoinstOfInterests { get; set; } = new List<PointOfInterestDto>();
     }
 }
