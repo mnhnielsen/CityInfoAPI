2ce95c7d066d4b74415d743c4f9b855f24981f99
mnhnielsen <mnhnielsen@gmail.com>
2022-09-07 21:39:33 +0200

    added entities

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<PointOfInterest> PointsOfInterest { get; set; } = new List<PointOfInterest>();
+
+        public City(string name)
+        {
+            Name = name;
+        }
+    }
+}
