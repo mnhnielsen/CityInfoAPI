daa67ba50b6ab72f86690bfd5b4b333c871ac7c0
mnhnielsen <mnhnielsen@gmail.com>
2022-09-07 21:41:33 +0200

    fields added

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -9,6 +9,9 @@ namespace CityInfo.API.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
 
         [ForeignKey("CityId")]
