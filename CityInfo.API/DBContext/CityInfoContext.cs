9cb77b3a1df5958fce252c1e21f4a2e1629cccd5
mnhnielsen <mnhnielsen@gmail.com>
2022-09-08 19:07:40 +0200

    sqlite init

---
+++ CityInfo.API/DBContext/CityInfoContext.cs
@@ -0,0 +1,22 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DBContext
+{
+    public class CityInfoContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; } = null!;
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+        public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
