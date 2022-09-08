a53b5633294297fecd1c22bcf3e68f780ad1de04
mnhnielsen <mnhnielsen@gmail.com>
2022-09-08 19:41:35 +0200

    database seeding

--- CityInfo.API/DBContext/CityInfoContext.cs
+++ CityInfo.API/DBContext/CityInfoContext.cs
@@ -13,10 +13,49 @@ namespace CityInfo.API.DBContext
 
         }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlite("connectionstring");
-        //    base.OnConfiguring(optionsBuilder);
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>()
+                .HasData(
+                new City("New York")
+                {
+                    Id = 1,
+                    Description = "The one with that big park"
+                },
+                new City("Berlin")
+                {
+                    Id = 2,
+                    Description = "The one with the good kebab"
+                },
+                new City("London")
+                {
+                    Id = 3,
+                    Description = "The one with that big tower with the clock"
+                }
+                );
+
+            modelBuilder.Entity<PointOfInterest>().
+                HasData(
+                new PointOfInterest("Central Park")
+                {
+                    Id = 1,
+                    CityId = 1,
+                    Description = "The most visited urban park in the United States."
+                },
+                new PointOfInterest("Checkpoint Charlie")
+                {
+                    Id = 2,
+                    CityId = 2,
+                    Description = "The most famous Berlin Wall checkpoints from the Cold War."
+                },
+                new PointOfInterest("New York Stock Exchange")
+                {
+                    Id = 3,
+                    CityId = 1,
+                    Description = "Many of the biggest and well known companies are traded publicly on this exchange."
+                }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
