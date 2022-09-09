176c84ea202b814560d74a765ca7f4e6d583f661
mnhnielsen <mnhnielsen@gmail.com>
2022-09-09 19:56:52 +0200

    contract

--- CityInfo.API/Services/ICityInfoRespository.cs
+++ CityInfo.API/Services/ICityInfoRespository.cs
@@ -6,5 +6,12 @@ namespace CityInfo.API.Services
     {
         //Async - freeing up threads asap for other tasks. Improves scalability.
         Task<IEnumerable<City>> GetCitiesAsync();
+
+        //Nullable with ?
+        Task<City?> GetCityAsync(int cityId);
+
+        Task<IEnumerable<PointOfInterest>> GetPointOfInterestForCityAsync(int cityId);
+
+        Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId);
     }
 }
