0043cd31ff2cc1a298f610f88cb44f280e1c9988
mnhnielsen <mnhnielsen@gmail.com>
2022-09-09 19:54:48 +0200

    repo init

---
+++ CityInfo.API/Services/ICityInfoRespository.cs
@@ -0,0 +1,10 @@
+﻿using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRespository
+    {
+        //Async - freeing up threads asap for other tasks. Improves scalability.
+        Task<IEnumerable<City>> GetCitiesAsync();
+    }
+}
