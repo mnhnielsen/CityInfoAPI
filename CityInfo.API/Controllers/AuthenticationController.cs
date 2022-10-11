388a9a8ed0a9ee1e3291b4453f6a787657385e73
mnhnielsen <mnhnielsen@gmail.com>
2022-10-11 09:20:37 +0200

    t init

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        public class AuthenticationRequestBody
+        {
+            public string? UserName { get; set; }
+            public string? Password { get; set; }
+        }
+
+        private class CityInfoUser
+        {
+            private int UserId { get; set; }
+            private string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+
+            public CityInfoUser(int userId, string userName, string firstName, string lastName, string city)
+            {
+                UserId = userId;
+                UserName = userName;
+                FirstName = firstName;
+                LastName = lastName;
+                City = city;
+            }
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+        {
+            var user = ValidateUserCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
+
+            if (user == null)
+                return Unauthorized();
+
+
+        }
+
+        private CityInfoUser ValidateUserCredentials(string? userName, string? password)
+        {
+            //Demo purpose, assume credentials are checked
+            return new CityInfoUser(
+                1,
+                userName?? "",
+                "Heinz",
+                "Ketchup",
+                "Antwerp");
+        }
+    }
+}
