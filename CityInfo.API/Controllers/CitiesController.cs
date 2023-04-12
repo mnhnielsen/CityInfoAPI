864b8b6228d19ce599806833c4a831f60070cfa1
mnhnielsen <mnhnielsen@gmail.com>
2023-04-12 22:33:16 +0200

    example updates

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -10,7 +10,7 @@ namespace CityInfo.API.Controllers
 {
     [ApiController]
     [Route("api/cities")]
-    [Authorize]
+    //[Authorize]
     public class CitiesController : ControllerBase
     {
         private readonly ICityInfoRepository _cityInfoRepository;
